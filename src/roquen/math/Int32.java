0ec3983a3ec8c11b85838119357b9d51915fecb6
roquendm <roquendm@gmail.com>
2014-01-27 14:33:49 +0100

    Initial commit

--- src/roquen/math/Int32.java
+++ src/roquen/math/Int32.java
@@ -3,6 +3,23 @@ package roquen.math;
 public final class Int32 {
   private Int32() {}
   
+  /** 
+   * Returns 1 for positive, 0 for zero and -1
+   * for negative inputs.
+   */
+  public static final int sign(int a)
+  {
+    return (-a >>> 31) | (a >> 31);
+  }
+  
+  /** 
+   * Returns 1 for input greater than zero, otherwise 0.
+   */
+  public static final int sgn(int a)
+  {
+    return (-a >>> 31);
+  }
+  
   /** ceiling(log2(x)) */
   public static final int ceilingLog2(int x)
   {
