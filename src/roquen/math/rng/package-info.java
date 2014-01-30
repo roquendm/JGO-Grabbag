f418b6e377299c54d277f2444cebbda87862fe7e
roquendm <roquendm@gmail.com>
2014-01-30 11:08:10 +0100

    Some verbage

--- src/roquen/math/rng/package-info.java
+++ src/roquen/math/rng/package-info.java
@@ -1,5 +1,9 @@
 /**
- * Blah
+ * This package contains a reasonable selection of pseudo random number
+ * generators targeting game runtimes.
+ * <p>
+ * For rational see: <a href=http://www.java-gaming.org/topics/pseudo-random-number-generators/27815/view.html">JGO wiki page</a>
+ * <link></link>
  */
 
 package roquen.math.rng;
