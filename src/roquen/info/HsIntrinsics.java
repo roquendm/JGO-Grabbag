1c315cdac2cb1baec104ee381ec50f4f2f8721e9
RoquenDM <roquendm@gmail.com>
2015-09-18 19:10:26 +0200

    Added a couple of SIMD tests

--- src/roquen/info/HsIntrinsics.java
+++ src/roquen/info/HsIntrinsics.java
@@ -3,90 +3,141 @@ package roquen.info;
 import roquen.math.Float32;
 
 // java -cp bin -XX:+UnlockDiagnosticVMOptions -XX:+TraceClassLoading -XX:+LogCompilation -XX:+PrintAssembly roquen.info.HsIntrinsics
-public class HsIntrinsics 
+
+//@SuppressWarnings("unused")
+public class HsIntrinsics
 {
   private static roquen.math.rng.XorStar64 rng = new roquen.math.rng.XorStar64();
- 
- 
-  // 1.8.0 (60) - performs the division (reasonable)
-  public static void spMulInvert(float[] d, float[] s)
+
+
+  // 1.8.0 (60) - performs the division.  Is generating divps.
+  private static void recip(float[] d, float[] s)
   {
     int len = d.length;
-    
+
     for(int i=0; i<len; i++) {
       d[i] = 1.f/s[i];
     }
   }
-  
+
   // 1.8.0 (60) - SSE converts to double, stores to memory,
   // load into x87, op, and back.
   //---
-  // 
-  public static void spSin(float[] d, float[] s)
+  //
+  private static void sin(float[] d, float[] s)
   {
     int len = d.length;
-    
+
     for(int i=0; i<len; i++) {
       d[i] = (float)Math.sin(s[i]);
     }
   }
-  
+
   // 1.8.0 (60) - does not fold, explicit individual calls to fsin & fcos
   // moving the parameter each time from SSE->Memory->x87.
-  public static void spSinCos(float[] d, float[] s)
+  private static void spSinCos(float[] d, float[] s)
   {
     int len = d.length>>1;
     int di  = 0;
-    
+
     for(int i=0; i<len; i++) {
       d[di++] = (float)Math.sin(s[i]);
       d[di++] = (float)Math.cos(s[i]);
     }
   }
-  
+
   // 1.8.0 (60) - moving back and forth between SSE and x87 via memory - ouch
   //---
-  // 
-  public static void spMultiX87(float[] d, float[] s)
+  //
+  private static void spMultiX87(float[] d, float[] s)
   {
     int len = d.length;
-    
+
     for(int i=0; i<len; i++) {
       d[i] = (float)(Math.cos((Math.PI/2.0)*Math.sin(s[i])));
     }
   }
-  
+
   // becomes a move, likewise for reverse
-  public static void spRawBits(float[] d, float[] s)
+  private static void getIntBit(float[] d, float[] s)
   {
     int len = d.length;
-    
+
     for(int i=0; i<len; i++) {
       d[i] = Float.floatToRawIntBits(s[i]);
     }
   }
-  
+
   // Single precision functions (including casting result) from Math
-  
-  public static void spLocalAbs(float[] d, float[] s)
+
+  private static void localAbs(float[] d, float[] s)
   {
     int len = d.length;
-    
+
     for(int i=0; i<len; i++) {
       d[i] = Float32.abs(s[i]);
     }
   }
-  
-  public static void spLocalMin(float[] d, float[] s)
+
+  private static void localMin(float[] d, float[] s)
   {
     int len = d.length;
     int si  = 0;
-    
+
     for(int i=0; i<len; i++) {
       d[i] = Float32.min(s[si++],s[si++]);
     }
   }
-  
+
+  /** generates: addps */
+  private static void sum(float[] d, float[] a, float[] b)
+  {
+    int len = d.length;
+
+    for(int i=0; i<len; i++) {
+      d[i] = a[i]+b[i];
+    }
+  }
+
+  /** does not generate addps  */
+  private static void sump4s(float[] d, float[] a, float[] b)
+  {
+    int len = d.length>>2;
+    int i   = 0;
+
+    while(i<len) {
+      d[i] = a[i]+b[i]; i++;
+      d[i] = a[i]+b[i]; i++;
+      d[i] = a[i]+b[i]; i++;
+      d[i] = a[i]+b[i]; i++;
+    }
+  }
+
+  /** does not generate addps  */
+  private static void sump4sa(float[] d, float[] a, float[] b)
+  {
+    int len = d.length>>2;
+    int i   = 0;
+
+    while(i<len) {
+      d[i  ] = a[i]  +b[i  ];
+      d[i+1] = a[i+1]+b[i+1];
+      d[i+2] = a[i+2]+b[i+2];
+      d[i+3] = a[i+3]+b[i+3];
+      i += 4;
+    }
+  }
+
+  // generates: mulps
+  private static void mul(float[] d, float[] a, float[] b)
+  {
+    int len = d.length;
+
+    for(int i=0; i<len; i++) {
+      d[i] = a[i]*b[i];
+    }
+  }
+
   // 1.8.0 (60) -
   //  movd r11d, xmm1
   //  movd r10d, xmm0
@@ -94,102 +145,136 @@ public class HsIntrinsics
   //  and  r10d, 0x80000000
   //  or   r10d, r11d
   //  movd xmm0, r10d
-  public static void spCopySign(float[] d, float[] s)
+  private static void copySign(float[] d, float[] s)
   {
     int len = d.length;
     for(int i=0; i<len; i++) { d[i] = Math.copySign(d[i],s[i]); }
   }
-  
+
   // branching as per java-source
-  public static void spAbs(float[] d, float[] s)
+  private static void abs(float[] d, float[] s)
   {
     int len = d.length;
     for(int i=0; i<len; i++) { d[i] = Math.abs(s[i]); }
   }
-  
+
   // converts to double..and standardish mess from there
-  public static void spFloor(float[] d, float[] s)
+  private static void floor(float[] d, float[] s)
   {
     int len = d.length;
     for(int i=0; i<len; i++) { d[i] = (float)Math.floor(s[i]); }
   }
-  
+
   // converts to double..and standardish mess from there
-  public static void spCeiling(float[] d, float[] s)
+  private static void ceiling(float[] d, float[] s)
   {
     int len = d.length;
     for(int i=0; i<len; i++) { d[i] = (float)Math.ceil(s[i]); }
   }
-  
+
   // perfectly sane
-  public static void spGetExp(int[] d, float[] s)
+  private static void getExp(int[] d, float[] s)
   {
     int len = d.length;
     for(int i=0; i<len; i++) { d[i] = Math.getExponent(s[i]); }
   }
-  
-  // transformed to: sqrtss
-  public static void spSqrt(float[] d, float[] s)
+
+  // transformed to: sqrtss (no sqrtps)
+  private static void sqrt(float[] d, float[] s)
   {
     int len = d.length;
     for(int i=0; i<len; i++) { d[i] = (float)Math.sqrt(s[i]); }
   }
-  
+
   private static final int SIZE = 1<<20;
-  
-  
-  public static void spBinaryOpDriver()
+
+
+  private static void floatOps()
   {
     float[] d = new float[SIZE];
     float[] s = new float[2*SIZE];
     int[]   di = new int[SIZE];
-    
+
     for(int i=0; i<2*SIZE; i++) {
       s[i] = 100.f*(rng.nextFloat()+1.f);
     }
-    
+
     for(int i=0; i<10; i++) {
-      spMulInvert(d,s);
-      spCopySign(d,s);
-      //spLocalMin(d,s);
-      spSin(d,s);
+      recip(d,s);
+      copySign(d,s);
+      localMin(d,s);
+      localAbs(d,s);
+      sin(d,s);
       spSinCos(d,s);
-      spFloor(d,s);
-      spCeiling(d,s);
-      spGetExp(di,s);
-      spSqrt(d,s);
+      floor(d,s);
+      ceiling(d,s);
+      getExp(di,s);
+      sqrt(d,s);
       spMultiX87(d,s);
+      abs(d, s);
+      sum(d,s,s);
+      sump4s(d,s,s);
+      sump4sa(d,s,s);
+      mul(d,s,s);
     }
   }
-  
-  
+
   // branching as per java-source
-  public static void dpAbs(double[] d, double[] s)
+  private static void abs(double[] d, double[] s)
   {
     int len = d.length;
     for(int i=0; i<len; i++) { d[i] = Math.abs(s[i]); }
   }
-  
-  
-  public static void dpBinaryOpDriver()
+
+
+  private static void doubleOps()
   {
     double[] d = new double[SIZE];
     double[] s = new double[2*SIZE];
-    
+
     for(int i=0; i<2*SIZE; i++) {
       s[i] = 100.f*(rng.nextDouble()+1.f);
     }
-    
+
     for(int i=0; i<10; i++) {
-      dpAbs(d, s);
+      abs(d, s);
     }
   }
-  
+
+
+  private static void max(int[] d, int[] s)
+  {
+    int len = d.length;
+    int si  = 0;
+    for(int i=0; i<len; i++) { d[i] = Math.max(s[si++],s[si++]); }
+  }
+
+  private static void min(int[] d, int[] s)
+  {
+    int len = d.length;
+    int si  = 0;
+    for(int i=0; i<len; i++) { d[i] = Math.max(s[si++],s[si++]); }
+  }
+
+  private static void intOps()
+  {
+    int[] d = new int[SIZE];
+    int[] s = new int[2*SIZE];
+
+    for(int i=0; i<2*SIZE; i++) {
+      s[i] = rng.nextInt();
+    }
+
+    for(int i=0; i<10; i++) {
+      max(d,s);
+      min(d,s);
+    }
+  }
+
   public static void main(String[] args)
   {
-    spBinaryOpDriver();
-    dpBinaryOpDriver();
+    floatOps();
+    doubleOps();
+    intOps();
   }
-  
-  
 }
